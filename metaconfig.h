34e83db8d5a4ced97fa58f78bdd1d114ec493dbb
artthirt <artthirt@gmail.com>
2017-08-08 06:59:03 +0300

    optimize code

--- metaconfig.h
+++ metaconfig.h
@@ -9,7 +9,7 @@ const int Boxes = 2;
 const int cnv_size = 4;
 const int mlp_size = 2;
 
-const int cnv_do_back_layers = 2;
+const int cnv_do_back_layers = 0;
 const int lrs = cnv_size - cnv_do_back_layers;
 
 const int first_classes = 0;
